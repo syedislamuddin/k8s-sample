7c07239a166761c841fcdb210f60a5d42beb6e53
syedislamuddin <70541292+syedislamuddin@users.noreply.github.com>
2025-06-24 22:06:01 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
 st.header(":green[**Hello, Streamlit!**]")
+st.write(":red[**Deployed by cloudbuild + argocd**]")
 st.write("This is a simple Streamlit app running in a Docker container.")
